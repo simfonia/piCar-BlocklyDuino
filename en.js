fd62ed504dc0899cf6694c9c606b26e2b27280c5
simfonia <lin.simfonia@gmail.com>
2025-10-05 12:35:54 +0800

    移除測試積木文本，並為英文及繁體中文類別檔案添加類別結構

--- en.js
+++ en.js
@@ -1,6 +1,5 @@
 // English language file
 Blockly.Msg["SIMFONIA_CATEGORY"] = "simfonia";
 Blockly.Msg["PICAR_CATEGORY"] = "piCar";
-Blockly.Msg["SIMFONIA_TEST_BLOCK_TEXT"] = "Simfonia Test Block";
 Blockly.Msg["SIMFONIA_HUE"] = "#5b33a5"; // A nice purple
 Blockly.Msg["PICAR_HUE"] = "#D9534F"; // A reddish color
