066de5ba18a829f7058f044fe1ce53f3d4950f4a
simfonia <lin.simfonia@gmail.com>
2025-10-06 13:29:10 +0800

    新增 piCar 相關積木，更新顏色與名稱，並清理工作區初始化邏輯

--- blocks.js
+++ blocks.js
@@ -1,5 +1,72 @@
+// 先清除原有的工作區所預設的積木
+// Use a global flag to ensure this cleanup runs only once.
+if (typeof window.simfoniaCleanupDone === 'undefined') {
+  window.simfoniaCleanupDone = true;
+  
+  // Wait for all initializations to complete, then clean up the workspace.
+  setTimeout(function() {
+    if (typeof Blockly !== "undefined" && Blockly.mainWorkspace) {
+      Blockly.mainWorkspace.clear();
+      newFile();
+    }
+  }, 1000); // Set the delay to 1 second
+}
 
 
+// 積木的外觀
+
+Blockly.Blocks['picar_init'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("初始化 piCar");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(Blockly.Msg["PICAR_SETUP_HUE"]);
+    this.setTooltip("初始化 piCar 所需的腳位與變數");
+    this.setHelpUrl("");
+  }
+};
+
+
+Blockly.Blocks['picar_set_hand_range'] = {
+  init: function() {
+    this.appendValueInput("RANGE")
+        .setCheck("Number")
+        .appendField("設定手臂開合範圍");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(Blockly.Msg["PICAR_SETUP_HUE"]);
+    this.setTooltip("設定手臂開合的角度範圍，預設 170°");
+    this.setHelpUrl("");
+  },
+  onchange: function(event) {
+    var rangeBlock = this.getInputTargetBlock('RANGE');
+    if (rangeBlock && rangeBlock.type === 'math_number') {
+      var numberValue = parseFloat(rangeBlock.getFieldValue('NUM'));
+      if (!isNaN(numberValue)) {
+        if (numberValue < 0) {
+          rangeBlock.setFieldValue('0', 'NUM');
+        } else if (numberValue > 180) {
+          rangeBlock.setFieldValue('180', 'NUM');
+        }
+      }
+    }
+  }
+};
+
+
+Blockly.Blocks['picar_resetPiCar'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("重新啟動 piCar");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(Blockly.Msg["PICAR_SETUP_HUE"]);
+    this.setTooltip("重新啟動 piCar");
+    this.setHelpUrl("");
+  }
+};
 
 
 Blockly.Blocks['picar_drive'] = {
@@ -48,7 +115,7 @@ Blockly.Blocks['picar_checkDistance'] = {
     this.appendDummyInput()
         .appendField("偵測距離 (cm)");
     this.setOutput(true, "Number");
-    this.setColour(Blockly.Msg["PICAR_HUE"]);
+    this.setColour(Blockly.Msg["COLOR_SENSOR_HUE"]);
     this.setTooltip("使用超音波偵測前方障礙物距離 (cm)");
     this.setHelpUrl("");
   }
@@ -59,34 +126,34 @@ Blockly.Blocks['picar_checkColor'] = {
     this.appendDummyInput()
         .appendField("偵測顏色 (黑0/白1)");
     this.setOutput(true, "Number");
-    this.setColour(Blockly.Msg["PICAR_HUE"]);
+    this.setColour(Blockly.Msg["COLOR_SENSOR_HUE"]);
     this.setTooltip("使用紅外線偵測地面顏色，回傳 0 為黑色，1 為白色");
     this.setHelpUrl("");
   }
 };
 
-Blockly.Blocks['picar_set_hand_range'] = {
+
+Blockly.Blocks['picar_inPosition'] = {
   init: function() {
-    this.appendValueInput("RANGE")
-        .setCheck("Number")
-        .appendField("設定手臂開合範圍");
-    this.setInputsInline(true);
+    this.appendDummyInput()
+        .appendField("手臂歸位");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(Blockly.Msg["PICAR_HUE"]);
-    this.setTooltip("設定手臂開合的角度範圍，預設 170°");
+    this.setColour(Blockly.Msg["SERVO_HUE"]);
+    this.setTooltip("手臂伺服馬達歸位");
     this.setHelpUrl("");
   }
 };
 
+
 Blockly.Blocks['picar_closeHands'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("閉合手臂");
+        .appendField("手臂合起");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(Blockly.Msg["PICAR_HUE"]);
-    this.setTooltip("閉合手臂");
+    this.setColour(Blockly.Msg["SERVO_HUE"]);
+    this.setTooltip("手臂合起");
     this.setHelpUrl("");
   }
 };
@@ -94,11 +161,11 @@ Blockly.Blocks['picar_closeHands'] = {
 Blockly.Blocks['picar_openHands'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("張開手臂");
+        .appendField("手臂張開");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(Blockly.Msg["PICAR_HUE"]);
-    this.setTooltip("張開手臂");
+    this.setColour(Blockly.Msg["SERVO_HUE"]);
+    this.setTooltip("手臂張開");
     this.setHelpUrl("");
   }
 };
@@ -109,61 +176,28 @@ Blockly.Blocks['picar_flashingLight'] = {
         .appendField("閃爍 RGB 燈");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(Blockly.Msg["PICAR_HUE"]);
+    this.setColour(Blockly.Msg["MEDIA_HUE"]);
     this.setTooltip("閃爍 RGB 燈");
     this.setHelpUrl("");
   }
 };
 
-Blockly.Blocks['picar_resetPiCar'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("重新啟動 piCar");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(Blockly.Msg["PICAR_HUE"]);
-    this.setTooltip("重新啟動 piCar");
-    this.setHelpUrl("");
-  }
-};
-
-Blockly.Blocks['picar_inPosition'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("手臂歸位");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(Blockly.Msg["PICAR_HUE"]);
-    this.setTooltip("手臂伺服馬達歸位");
-    this.setHelpUrl("");
-  }
-};
 
 Blockly.Blocks['picar_easterEgg'] = {
   init: function() {
     this.appendValueInput("TEMPO")
         .setCheck("Number")
-        .appendField("播放彩蛋旋律 節奏");
+        .appendField("播放彩蛋旋律 全音符的時間 (ms)");
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(Blockly.Msg["PICAR_HUE"]);
+    this.setColour(Blockly.Msg["MEDIA_HUE"]);
     this.setTooltip("播放一段旋律，tempo 數值愈大愈快");
     this.setHelpUrl("");
   }
 };
 
-Blockly.Blocks['picar_init'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("初始化 piCar");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(Blockly.Msg["PICAR_HUE"]);
-    this.setTooltip("初始化 piCar 所需的腳位與變數");
-    this.setHelpUrl("");
-  }
-};
+
 
 Blockly.Blocks['picar_rawCode'] = {
   init: function() {
@@ -178,7 +212,7 @@ Blockly.Blocks['picar_rawCode'] = {
     // 設置外觀和連接
     this.setPreviousStatement(true, null); // 允許前面連接
     this.setNextStatement(true, null);     // 允許後面連接
-    this.setColour(Blockly.Msg["PICAR_HUE"]);
+    this.setColour(Blockly.Msg["ADVANCED_HUE"]);
     this.setTooltip("可填入任意程式碼或註解");
     this.setHelpUrl("");
     
--- zh-hant.js
+++ zh-hant.js
@@ -1,5 +1,10 @@
 // Traditional Chinese language file
 Blockly.Msg["SIMFONIA_CATEGORY"] = "simfonia";
-Blockly.Msg["PICAR_CATEGORY"] = "piCar";
+Blockly.Msg["PICAR_CATEGORY"] = "πCar";
 Blockly.Msg["SIMFONIA_HUE"] = "#5b33a5"; // A nice purple
-Blockly.Msg["PICAR_HUE"] = "#D9534F"; // A reddish color
+Blockly.Msg["PICAR_HUE"] = "#ee5b56"; // for piCar movement
+Blockly.Msg["PICAR_SETUP_HUE"] = "#0872e2"; // for initialization
+Blockly.Msg["COLOR_SENSOR_HUE"] = "#b1b100"; // for sensor
+Blockly.Msg["SERVO_HUE"] = "#07a91d"; // for servo
+Blockly.Msg["MEDIA_HUE"] = "#de57ad"; // for multimedia
+Blockly.Msg["ADVANCED_HUE"] = "#556f79"; // for advanced coding
